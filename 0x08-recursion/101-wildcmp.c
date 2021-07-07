ff94fbd9c9139a16019e49d4c64fb20cdf450ecb
akandeav <akandevic@gmail.com>
2021-07-07 21:43:41 +0100

    Correctione due

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,54 +1,66 @@
 #include "holberton.h"
-int len(char *s);
-int cmp(char *s1, char *s2, int len1, int len2, int i, int j);
+
 /**
- * len - returns the length of the string.
- * @s: string pointer.
- * Return: string length.
+ * test - tests if wildcard is a proper match
+ * @s5: string to compare
+ * @s6: wildcard match to compare
+ * @t5: first spot that was checked
+ * @t6: first spot in wildcard comparison
+ *
+ * Return: spot that matches or NULL
  */
-int len(char *s)
+char *test(char *s5, char *s6, char *t5, char *t6)
 {
-	if (*s != '\0')
-	{
-		return (len(s + 1) + 1);
-	}
-	return (0);
+	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
+		return (t5);
+	else if (*s5 != *s6)
+		return (wildcard(s5, t6));
+	else
+		return (test(s5 + 1, s6 + 1, t5, t6));
 }
 /**
- * wildcmp - compare 2 strings.
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if strings are identical otherwise 0.
+ * wildcard - checks for the wildcards
+ * @s3: string to compare to
+ * @s4: string to check
+ *
+ * Return: pointer to spot in s3 that matches s4
+ */
+char *wildcard(char *s3, char *s4)
+{
+	if (*s3 != *s4)
+	{
+		if (*s3 == '\0')
+			return (0);
+		return (wildcard(s3 + 1, s4));
+	}
+	return (test(s3 + 1, s4 + 1, s3, s4));
+}
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string, may contain wildcard *
+ *
+ * Return: 1 if match, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
-	int len1 = len(s1);
-	int len2 = len(s2);
+	char *p;
 
-	return (cmp(s1, s2, len1 - 1, len2 - 1, 0, 0));
-}
-/**
- * cmp - compare strings.
- * @s1: string 1
- * @s2: string 2
- * @len1: string 1 length
- * @len2: string 2 length
- * @i: s1 counter
- * @j: s2 counter
- * Return: 1 or 0
- */
-int cmp(char *s1, char *s2, int len1, int len2, int i, int j)
-{
-	if (s1[i] == s2[j])
-		return (cmp(s1, s2, len1, len2, i + 1, j + 1));
-	else if (s1[i] != s2[j] && s2[j] != '*')
+	if (*s2 == '*' && *(s2 + 1) == '\0')
+		return (1);
+	else if (*s2 == '*' && *(s2 + 1) == '*')
+		return (wildcmp(s1, s2 + 1));
+	else if (*s2 == '*')
+	{
+		p = wildcard(s1, s2 + 1);
+		if (p == 0)
+			return (0);
+		return (wildcmp(p + 1, s2 + 2));
+	}
+	else if (*s1 != *s2)
 		return (0);
-	else if (s1[i] != s2[j] && s2[j] == '*' && s1[i + 1] == s2[j + 1])
-		return (cmp(s1, s2, len1, len2, i + 1, j + 1));
-	else if (s1[i] != s2[j] && s2[j] == '*')
-		return (cmp(s1, s2, len1, len2, i + 1, j));
-	else if (s1[i] == '\n')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else
-		return (1);
+	return (wildcmp(s1 + 1, s2 + 1));
 }
