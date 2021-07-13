eeaef84cefbea1e79a9ff60a30f81fc484796d17
akandeav <akandevic@gmail.com>
2021-07-13 21:24:10 +0100

    Correction again

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,42 +1,40 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
+
 /**
- * argstostr - concatenates all arguments of your program.
- * @ac: arguments count.
- * @av: arguments vector.
- * Return: pointer to new string or NULL if failed.
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: number of arguments
+ * @av: double pointer to arguments
+ *
+ * Return:pointer to new string, or NULL if error
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, c, size = 0;
-	char *p;
+	int i, j, k = 0, n = 0;
+	char *s;
 
-	if (ac == 0 || av == NULL)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			size++;
-		}
+		for (j = 0; av[i][j]; j++)
+			n++;
+		n++;
 	}
-	p = malloc(sizeof(char) * size + 1 + ac);
-	if (p == NULL)
-	{
-		free(p);
+	n++;
+	s = malloc(n * sizeof(char));
+	if (s == NULL)
 		return (NULL);
-	}
-	c = 0;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			p[c] = av[i][j];
-			c++;
+			s[k] = av[i][j];
+			k++;
 		}
-		p[c] = '\n';
-		c++;
+		s[k] = '\n';
+		k++;
 	}
-	p[size + size] = '\0';
-	return (p);
+	s[k] = '\0';
+	return (s);
 }
